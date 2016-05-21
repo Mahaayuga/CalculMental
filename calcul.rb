8c87566aca259537fb7328446e8e3138a86f1ece
Mahaayuga <benoit.mazure@gmail.com>
2016-05-21 13:05:15 +0200

    fix du délai de réponse

--- calcul.rb
+++ calcul.rb
@@ -9,15 +9,17 @@ loop do
   b = rand(2..level[:max_b])
 
   deb = Time.now
-  print "#{a} x #{b} = "
+  print " #{a} x #{b} = "
   reponse = gets.chomp
   fin     = Time.now
 
   break if reponse == stop
+  level[:tmp_fail] += 1 if fin - deb >= 5  #application d'un malus si la réponse est trop longue
+  
   if reponse.to_i == a * b
 
     level[:tmp_success] += 1
-    level[:tmp_fail] = 0
+#   level[:tmp_fail] = 0  #suppression du raz du fail
      
     if level[:tmp_success] == 3
       level[:max_a] += 1 unless level[:max_a] == 20
@@ -26,26 +28,26 @@ loop do
     end
 
   else
-    printf "# => %i \n", a * b
-    score[:err]      += 1
+    printf " # => %i \n", a * b
+    score[:err] += 1
 
-    level[:tmp_fail] += 1
+    level[:tmp_fail]   += 1
     level[:tmp_success] = 0
-
-    unless ( level[:tmp_fail] == 2 ) or ( fin - deb >= 10 )
-      level[:max_a] -= 1 unless level[:max_a] == 5
-      level[:max_b] -= 1 unless level[:max_b] == 5
-      level[:tmp_fail] == 0    
-    end
 
   end
   
+  #Baisse de la difficulté si trop de fails consécutifs
+  if level[:tmp_fail] == 2
+    level[:max_a] -= 1 unless level[:max_a] == 5
+    level[:max_b] -= 1 unless level[:max_b] == 5
+    level[:tmp_fail] == 0    
+  end 
+  
+  #incrémentation
   score[:time] += fin - deb
-  score[:try] += 1
+  score[:try]  += 1
 
 end
-
-
 
 unless score[:try] == 0
   printf "\n\tAu revoir !\n" \
